aca72a52a443e0cc15714eec7a715f484c89ee18
unknown <xoumanivongco@gmail.com>
2023-09-02 17:51:01 -0600

    week9 homework edits

--- src/main/resources/project.schema.sql
+++ src/main/resources/project.schema.sql
@@ -12,37 +12,37 @@ CREATE TABLE project_category(
 );
 
 CREATE TABLE category(
-	category_id INT NOT NULL,
+	category_id INT AUTO_INCREMENT NOT NULL,
 	category_name VARCHAR(128) NOT NULL,
-	PRIMARY KEY (category_id) AUTO_INCREMENT
+	PRIMARY KEY (category_id)
 );
 
 CREATE TABLE step(
-	step_id INT NOT NULL,
+	step_id INT AUTO_INCREMENT NOT NULL,
 	project_id INT NOT NULL,
 	step_text TEXT NOT NULL,
 	step_order INT NOT NULL,
-	PRIMARY KEY (step_id) AUTO_INCREMENT,
+	PRIMARY KEY (step_id),
 	FOREIGN KEY (project_id) REFERENCES project (project_id)ON DELETE CASCADE
 );
 
 CREATE TABLE material(
-	material_id INT NOT NULL,
+	material_id INT AUTO_INCREMENT NOT NULL,
 	project_id INT NOT NULL,
 	material_name VARCHAR(128),
 	num_required INT,
 	cost DECIMAL(7,2),
-	PRIMARY KEY (material_id) AUTO_INCREMENT,
+	PRIMARY KEY (material_id),
 	FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
 );
 
 CREATE TABLE project(
-	project_id INT NOT NULL,
+	project_id INT AUTO_INCREMENT NOT NULL,
 	project_name VARCHAR(128) NOT NULL,
 	estimated_hours DECIMAL (7,2),
 	actual_hours DECIMAL (7,2),
 	difficulty INT,
 	notes TEXT,
-	PRIMARY KEY (project_id) AUTO_INCREMENT
+	PRIMARY KEY (project_id) 
 	
 );
